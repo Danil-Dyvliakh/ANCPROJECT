c74971f687d4d44adc2f33a7a9021f55b559ea91
Danil <dandy0957@gmail.com>
2024-02-14 18:16:32 +0200

    Повне закінення добавлення людини через форму регестрації

--- crm/forms.py
+++ crm/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class PeopleForm(forms.Form):
-    order_name = forms.CharField(max_length=200, label='ПІБ')
-    order_position = forms.CharField(max_length=200, label='Посада')
-    order_email = forms.EmailField(max_length=200, label='Email')
+    name = forms.CharField(max_length=200, label='ПІБ', required = False)
+    position = forms.CharField(max_length=200, label='Посада')
+    email = forms.EmailField(max_length=200, label='Email')
